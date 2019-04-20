3bb7d87e6dd1416cea1b749bb46b8537d971370a
Matthew W <fatboychummy@gmail.com>
2019-04-19 22:04:47 -0600

    Moved some checks from listen to init

--- modules/Chat.lua
+++ modules/Chat.lua
@@ -1,37 +1,17 @@
 --[[FATFILE
-1
+2
 https://raw.githubusercontent.com/fatboychummy/Modu/Master/modules/Chat.lua
+
+This module controls all chat interaction
 ]]
 
 local funcs = {}
 local listenFor = "chat_message"
 local player = ""
+local manip = false
 
 function funcs.listen()
-  local manip = {peripheral.find("manipulator")}
 
-  if #manip > 1 then
-    error("Too many manipulators connected to the network.\nThis program currently will not support multiple manipulators.")
-  elseif #manip == 0 then
-    error("No manipulator (or manipulator has no modules)!")
-  else
-    print("Single manipulator.")
-    manip = manip[1]
-  end
-
-  local modules = manip.listModules()
-  local chatPresent = false
-  for i = 1, #modules do
-    if modules[i] == "plethora:chat"
-      or modules[i] == "plethora:chat_creative" then
-
-      print("Chat recorder is available.")
-      chatPresent = true
-    end
-  end
-  if not chatPresent then
-    error("No chat recorder present.")
-  end
 
   while true do
     local ev = {os.pullEvent(listenFor)}
@@ -42,11 +22,36 @@ function funcs.listen()
 end
 
 function funcs.init(owner)
+  -- handle the owner
   player = owner or false
   if player then
     return true
+  else
+    return false, "Owner not specified (edit InitData and change or add variable 'owner'!)."
   end
-  return false, "Owner not specified (edit Modu and change the owner!)."
+
+  -- handle the manipulator
+  manip = {peripheral.find("manipulator")}
+
+  if #manip > 1 then
+    return false, "Too many manipulators connected to the network.\nThis program currently will not support multiple manipulators."
+  elseif #manip == 0 then
+    return false, "No manipulator (or manipulator has no modules)!"
+  else
+    manip = manip[1]
+  end
+
+  -- handle the manipulator modules
+  local modules = manip.listModules()
+
+  for i = 1, #modules do
+    if modules[i] == "plethora:chat"
+      or modules[i] == "plethora:chat_creative" then
+      return true
+    end
+  end
+
+  return false, "No chat recorder present."
 end
 
 
