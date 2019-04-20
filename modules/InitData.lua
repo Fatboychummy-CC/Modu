12fc73f125a2436358227511a8ddb584242e8c53
Matthew W <fatboychummy@gmail.com>
2019-04-19 22:05:30 -0600

    Added InitData (previous commit adds a fix for this in Chat.lua)

---
+++ modules/InitData.lua
@@ -0,0 +1,12 @@
+--[[FATFILE
+1
+https://raw.githubusercontent.com/fatboychummy/Modu/Master/modules/InitData.lua
+
+This module controls what data is passed to each of the `init` functions
+]]
+
+local data = {
+  owner = "PlayerName"
+}
+
+return data
