745c1f03df093796844a82eaffb09a8c1f83b46f
Matthew W <fatboychummy@gmail.com>
2019-04-24 23:22:03 -0600

    Testing commit

--- modules/Control.lua
+++ modules/Control.lua
@@ -7,20 +7,41 @@ This module is essentially the "main" function in the program.
 
 local funcs = {}
 
+local mods = {}
+
 function funcs.go(modules)
+  -- error check
   if type(modules) ~= "table" then
     error("Bad argument (expected table, got " .. type(modules) .. ").", 2)
   end
 
+  -- change the names of the modules to be shorter
+  for k, v in pairs(modules) do
+    modules[k:match("%.(.+)")] = modules[k]
+    print(k:match("%.(.+)"))
+    modules[k] = nil
+  end
+
+  -- set the local values for interaction between funcs
+  mods.interactor = modules["Chat.PlayerInteraction"]
+  mods.parser = modules["Chat.Parser"]
+  mods.listener = modules["Chat.Listener"]
+
+  -- set this chunk's stuff
+  local interactor = mods.interactor
+  local parser = mods.parser
+  local listener = mods.listener
+
+  interactor.tell("Ready.")
+  error("oh god oh fuck")
 end
 
 function funcs.err(err)
-  -- none of this will work.
-  pcall(modules.Chat.tell, "------------------------------")
-  pcall(modules.Chat.tell, "Modu has stopped unexpectedly.")
-  pcall(modules.Chat.tell, err)
-  pcall(modules.Chat.tell, "Please report this to Fatboychummy#4287 on Discord")
-  pcall(modules.Chat.tell, "------------------------------")
+  pcall(mods.interactor.tell, "------------------------------")
+  pcall(mods.interactor.tell, "Modu has stopped unexpectedly.")
+  pcall(mods.interactor.tell, err)
+  pcall(mods.interactor.tell, "Please report this to Fatboychummy#4287 on Discord")
+  pcall(mods.interactor.tell, "------------------------------")
 end
 
 function funcs.init(data)
