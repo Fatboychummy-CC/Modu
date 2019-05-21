50794347bccd91a25d7fd92b00af54933389866b
Matthew W <fatboychummy@gmail.com>
2019-05-21 00:44:27 -0600

    Added small, but helpful, functions

---
+++ modules/Item/ItemModules/Manipulators.lua
@@ -0,0 +1,35 @@
+--[[FATFILE
+1
+https://raw.githubusercontent.com/fatboychummy/Modu/Master/modules/Item/ItemModules/Manipulators.lua
+
+This module controls manipulator stuff
+]]
+
+local funcs = {}
+
+function funcs.getFunctionFromManip(func)
+  local manips = {peripheral.find("manipulator")}
+  if #manips > 0 then
+    for i = 1, #manips do
+      local manip = manips[i]
+      for k, v in pairs(manip) do
+        if k == func then
+          return v
+        end
+      end
+    end
+  else
+    error("No manipulators connected.")
+  end
+  return false
+end
+
+function funcs.getFunctionsFromManip(funcs)
+  local all = {}
+  for i = 1, #funcs do
+    all[i] = funcs.getFunctionFromManip(funcs[i])
+  end
+  return all
+end
+
+return funcs
