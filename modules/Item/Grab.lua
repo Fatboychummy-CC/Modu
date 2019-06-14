23f3851328916ec7e59e35a4c3578d11cdf42ab4
Matthew W <fatboychummy@gmail.com>
2019-06-14 02:11:55 -0600

    The finishing punch

--- modules/Item/Grab.lua
+++ modules/Item/Grab.lua
@@ -7,32 +7,49 @@ This module is the main module
 
 local funcs = {}
 
-local inv = require("modules.Item.ItemModules.Inventory")
+local inv, cacher, manip, pInv
 
 function funcs.go(modules, vars)
   local interactor = modules["Core.Interaction.PlayerInteraction"]
-
-  local function tAssert(statement, error)
-    if not statement then
-      interactor.tell("Error:" .. error)
-      return true
-    end
-    return false
-  end
-
+  local tell = interactor.tell
+  print(textutils.serialize(vars))
   local item = vars[2]
   local count = vars[3]
-  local dd = vars[4]
-  local searchTypeFlag = true
+  local damage = vars[4]
+  count = tonumber(count)
+  damage = tonumber(damage)
 
-  if type(vars[4]) == "string" and vars[4]:lower() == "true" then
-    searchTypeFlag = false
+  if not item then
+    tell("Expected item name.")
+    return
   end
 
-  if searchTypeFlag then
-    -- if using modname:item_id + damage search type ...
+  if vars.flags['s'] then
+    tell("Caching... This may take some time depending on the attached "
+          .. "network.")
+    local h = cacher.scan()
+    tell("Done. Added " .. tostring(h) .. " new item(s).")
+  end
+
+  if count then
+    if not damage then
+      -- cacher.calculateReverseCache()
+      local rCache = cacher.getReverseCache()
+      local itm = rCache[string.lower(item)]
+      if itm then
+        damage = itm.d
+        item = itm.i
+      else
+        tell("No cache hit, try using the flag 's' to force a cache update.")
+        return
+      end
+    end
+    tell("Sending items.")
+    local ctn = inv.grabItemsFromAll(pInv(), count, item, damage)
+    tell("Found " .. tostring(ctn) .. "/" .. tostring(count) .. " items.")
   else
-    -- if using "The item's name" search type ...
+    tell("Expected item count.")
+    return
   end
 
 end
@@ -48,20 +65,15 @@ function funcs.help()
     "  get minecraft:dye 32 4",
     "    transfers lapis lazuli to your inventory.",
     "",
-    "  get \"lapis lazuli\" 10 -v",
-    "    transfers an item via it's screen name.  Note that this is very slow.",
+    "  get \"lapis lazuli\" 10",
+    "    transfers an item via it's screen name.",
     "",
-    "  This module accepts the following as numbers: \"all\" and \"every\".",
-    "  Example:",
-    "    get minecraft:dye all",
-    "  Will grab every single item in the storage network named minecraft:dye."
-      .. " This is the similar to:",
-    "    get \"lapis lazuli\" all -v",
-    "  As the above will grab all items named \"lapis lazuli\".",
+    "If a damage value is supplied, then Modu will only search via"
+      .. " modname:itemname and damage, otherwise it will only search "
+      .. "\"Via the full name\"",
     "",
     "Flags:",
-    "  s: Switch between indexing by an item's 'modname:item_name' "
-      .. "and 'full name'"
+    "  s: Scan and update the cache before searching."
   }
 end
 
@@ -74,6 +86,11 @@ function funcs.getInfo()
 end
 
 function funcs.init(data)
+  inv = require("modules.Item.ItemModules.Inventory")
+  cacher = require("modules.Item.ItemModules.Cacher")
+  manip = require("modules.Item.ItemModules.Manipulators")
+  pInv = manip.getFunctionFromManip("getInventory")
+
   return true
 end
 
