838f3f5c5325bb9fa6535077d7694581b84013c6
fatboy <fatboychummy@gmail.com>
2020-02-20 22:26:46 -0700

    fuck

--- modules/Optional/Execute.lua
+++ modules/Optional/Execute.lua
@@ -1,6 +1,6 @@
 --[[FATFILE
 1
-https://raw.githubusercontent.com/fatboychummy/Modu/Master/modules/Optional/Execute.lua
+https://raw.githubusercontent.com/fatboychummy/Modu/master/modules/Optional/Execute.lua
 
 This module executes lua code directly.
 ]]
